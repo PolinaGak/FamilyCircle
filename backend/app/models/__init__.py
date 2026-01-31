186737ba10e0d1e6ad21df8c76cc5321ecc052d6
Polina <polinagakgak@yandex.ru>
2026-01-31 15:52:43 +0300

    git commit -m "Add classes to init"

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,20 @@
+from .user import User
+
+from .family import Family
+from .family_member import FamilyMember
+
+from .relationship import Relationship
+
+from .invitation_code import InvitationCode
+
+from .album import Album
+from .album_member import AlbumMember
+from .photo import Photo
+
+from .chat import Chat
+from .chat_member import ChatMember
+from .message import Message
+
+from .event import Event
+from .event_participant import EventParticipant
+
